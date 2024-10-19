e137f8dc4a5dc4f4a59ed133355d680f8ce120e6
changzhiho <changzhiho@hotmail.com>
2024-10-19 12:11:07 +0200

    class bank

---
+++ Bank/Bank.cs
@@ -0,0 +1,37 @@
+class Bank
+{
+    public Dictionary<string, CurrentAccount> Accounts { get; }
+    public string Name { get; }
+
+    public Bank(string name)
+    {
+        Name = name;
+        Accounts = new Dictionary<string, CurrentAccount >();
+        
+    }
+
+    public void AddAccount(CurrentAccount account)
+    {
+        if (Accounts.TryAdd(account.Number, account))
+        {
+            Console.WriteLine("Le compte existe déjà");
+        }
+        else
+        {
+            Accounts.Add(account.Number, account);
+            Console.WriteLine($"Le compte {account.Number} a été ajouté");
+        }
+    }
+
+    public void DeleteAccount(string number)
+    {
+        if (Accounts.Remove(number))
+        {
+            Console.WriteLine($"Le compte {number} a été supprimé");
+        }
+        else
+        {
+            Console.WriteLine("Compte non trouvé");
+        }
+    }
+}
--- Bank/Program.cs
+++ Bank/Program.cs
@@ -1,2 +1,3 @@
 ﻿Personne p = new("Anthony", "Chang", new DateTime(1997, 5, 5));
 CurrentAccount ca = new("123456", 1000, 500, p);
+Bank bank = new("ING");
